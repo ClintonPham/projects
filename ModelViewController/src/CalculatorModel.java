4f5a99f5cd4dd56d7e5068d23acebbbf78395dc3
ClintonPham <55491604+ClintonPham@users.noreply.github.com>
2019-09-22 00:09:22 +1000

    MVC - Basic Calculator. MVC Refreshner

---
+++ ModelViewController/src/CalculatorModel.java
@@ -0,0 +1,17 @@
+
+public class CalculatorModel {
+	
+	private int calculationValue;
+	
+	public void addTwoNuumbers(int firstNumber, int secondNumber) {
+		
+		calculationValue = firstNumber + secondNumber;
+	}
+	
+	public int getCalculationValue() {
+		return calculationValue;
+	}
+
+}
+
+// Contains the data
---
+++ ModelViewController/src/CalculatorView.java
@@ -0,0 +1,58 @@
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class CalculatorView extends JFrame {
+	
+	private JTextField firstNumber = new JTextField(10);
+	private JLabel additionLabel = new JLabel("+");
+	private JTextField secondNumber = new JTextField(10);
+	private JLabel equalsLabel = new JLabel("=");
+	private JButton calculateButton = new JButton("Calculate");
+	private JTextField calcSolution = new JTextField(10);
+	
+	CalculatorView(){
+		
+		JPanel calcPanel = new JPanel();
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(600, 200);
+		
+		calcPanel.add(firstNumber);
+		calcPanel.add(additionLabel);
+ 		calcPanel.add(secondNumber);
+ 		calcPanel.add(equalsLabel);
+		calcPanel.add(calcSolution);
+		calcPanel.add(calculateButton);
+
+		
+		this.add(calcPanel);
+	}
+	
+	public int getFirstNumber() {
+		return Integer.parseInt(firstNumber.getText());
+	}
+
+	public int getSecondNumber() {
+		return Integer.parseInt(secondNumber.getText());
+	}
+	
+	public int getCalcSolution() {
+		return Integer.parseInt(calcSolution.getText());
+	}
+	
+	public void setCalcSolution(int solution) {
+		calcSolution.setText(Integer.toString(solution));
+		
+	}
+	
+	void addCalculationListener(ActionListener listenForCalcButton) {
+		
+		calculateButton.addActionListener(listenForCalcButton);
+		
+	}
+	
+	void displayErrorMessage(String errorMessage) {
+		JOptionPane.showMessageDialog(this, errorMessage);
+	}
+}
